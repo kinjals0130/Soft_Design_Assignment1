a7086ac3854c66ff18eed7645cd582b84360d8fe
Foram <foram4444@gmail.com>
2021-09-19 22:51:47 -0400

    Delete TestFactoryPattern.java

--- src/TestFactoryPattern.java
+++
@@ -1,7 +0,0 @@
-public class TestFactoryPattern {
-    public static void main (String [] args){
-        AbstractFactory productFactory = new ProductFactory();
-        Product honda = productFactory.createProduct("honda");
-        Product toyota = productFactory.createProduct("toyota");
-    }
-}
